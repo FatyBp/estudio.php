bed42d1f310b05dcfcb447787cfe6f152773f629
FatyBp <93344788+FatyBp@users.noreply.github.com>
2022-06-16 21:56:03 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!-- <form action ="" ->da una accion en este caso al boton 
+    method="POST>
+    <input type="text" placeholder="escribe algo">
+<br>
+<button></button> -->
+<html>
+    <form action="servidor.php" method="POST">
+        <input type"text" name="campo1"
+        placeholder ="escribe algo">
+        <br>
+        checar
+        <input type="checkbox" name="checar">
+        <br>
+        <input type="radio"name="r" value="1">
+        <input type="radio"name="r" value="2">
+        <br>
+        enviar
+
+    </form>
+</html>
---
+++ sevidor.php
@@ -0,0 +1,8 @@
+<?php
+$chechar=0;
+    $campo = $_POST['campo1'];
+    if (isset($_POST['checar'])){
+        $chechar=1;
+    }
+    echo $checar;
+?>
